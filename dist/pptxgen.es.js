63738a37fb8903bd1ccc13a47fdb484234987444
Mike Hollibaugh <mike.hollibaugh@pitchly.com>
2024-06-20 08:33:00 -0700

    Revert "test bullet color"
    This reverts commit 65a9062aae1eb9478ad238a0aec9a7c64c0b24f6.

--- dist/pptxgen.es.js
+++ dist/pptxgen.es.js
@@ -2132,7 +2132,8 @@ function genXmlParagraphProperties(textObj, isDefault) {
             paragraphPropXml += " marL=\"" + (textObj.options.indentLevel && textObj.options.indentLevel > 0 ? bulletMarL + bulletMarL * textObj.options.indentLevel : bulletMarL) + "\" indent=\"-" + bulletMarL + "\"";
             strXmlBullet = "<a:buSzPct val=\"100000\"/><a:buChar char=\"" + BULLET_TYPES['DEFAULT'] + "\"/>";
             if (textObj.options.bullet.color) {
-                strXmlBullet += '<a:buClr><a:srgbClr val="00ff00"/></a:buClr>';
+                strXmlBullet += '<a:buClr><a:srgbClr val="' + textObj.options.bullet.color + '"/></a:buClr>';
+                console.log('bullet', strXmlBullet);
             }
         }
         else if (textObj.options.bullet === false) {
