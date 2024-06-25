a84f0d96e257f040ac85b7a8d78cfec7c624e252
Mike Hollibaugh <mike.hollibaugh@pitchly.com>
2024-06-25 08:22:47 -0700

    remove tests

--- dist/pptxgen.cjs.js
+++ dist/pptxgen.cjs.js
@@ -1393,7 +1393,6 @@ function slideObjectToXml(slide) {
     }
     // STEP 3: Continue slide by starting spTree node
     strSlideXml += '<p:spTree>';
-  strSlideXml += '<p:grpSp>';
     strSlideXml += '<p:nvGrpSpPr><p:cNvPr id="1" name=""/><p:cNvGrpSpPr/><p:nvPr/></p:nvGrpSpPr>';
     strSlideXml += '<p:grpSpPr><a:xfrm><a:off x="0" y="0"/><a:ext cx="0" cy="0"/>';
     strSlideXml += '<a:chOff x="0" y="0"/><a:chExt cx="0" cy="0"/></a:xfrm></p:grpSpPr>';
@@ -1958,11 +1957,9 @@ function slideObjectToXml(slide) {
         strSlideXml += '</p:txBody></p:sp>';
     }
     // STEP 6: Close spTree and finalize slide XML
-  strSlideXml += '</p:grpSp>';
     strSlideXml += '</p:spTree>';
     strSlideXml += '</p:cSld>';
     // LAST: Return
-  console.log(strSlideXml);
     return strSlideXml;
 }
 /**
